ec084e1defc7d054a5b6d699c368d6c7b97e2d02
littekge <littekge@miamioh.edu>
2026-08-08 16:58:31 -0500

    feat: started work on TextView to display notes
    ## 2026-08-08 — Beginning of Notes Display
    
    - **Defined the `buildNoteBox()` function** to create a note box that
    will eventually display rendered markdown text.
    - Seperated the box building code into a seperate function `buildBox()`
    which is currently unused.

--- internal/ui/app.go
+++ internal/ui/app.go
@@ -27,16 +27,30 @@ func setDrawVars() {
 	tview.Borders.BottomRightFocus = tview.BoxDrawingsLightArcUpAndLeft
 }
 
-func BuildApp() {
-	setDrawVars()
-
-	app = tview.NewApplication()
+func buildBox() *tview.Box {
 	box := tview.NewBox().
 		SetBorder(true).
-		SetTitle("test").
-		SetBorderStyle(LazyNotesBlurStyle())
+		SetTitle("Note")
 	box.SetFocusFunc(func() { box.SetBorderStyle(LazyNotesFocusStyle()) })
 	box.SetBlurFunc(func() { box.SetBorderStyle(LazyNotesBlurStyle()) })
+	return box
+}
+
+func buildNoteBox() *tview.TextView {
+	tv := tview.NewTextView().
+		SetDynamicColors(true).
+		SetRegions(true).
+		SetText("test text")
+	tv.SetBorder(true)
+	tv.SetFocusFunc(func() { tv.SetBorderStyle(LazyNotesFocusStyle()) })
+	tv.SetBlurFunc(func() { tv.SetBorderStyle(LazyNotesBlurStyle()) })
+	return tv
+}
+
+func BuildApp() {
+	setDrawVars()
+	box := buildNoteBox()
+	app = tview.NewApplication()
 	if err := app.SetRoot(box, true).Run(); err != nil {
 		panic(err)
 	}
