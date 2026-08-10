15640f81898e86ead3725f31fb2f8214dd8e23d1
littekge <littekge@miamioh.edu>
2026-08-10 13:58:34 -0400

    feat: add basic file parser
    - **Added file parser** to read markdown files and convert them into a
    displayable string.
    - parser only accepts file paths that reference real files with the .md
    extension.

---
+++ internal/parser/parse.go
@@ -0,0 +1,23 @@
+// Package parser reads from a markdown file (.md) and formats the contents of the file in accordance to the tview text format.
+package parser
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+)
+
+func ParseText(path string) (string, error) {
+	// Ensure that 'path' is the path to an existing markdown file
+	if filepath.Ext(path) != ".md" {
+		return "", errors.New("parser: invalid file extension" + filepath.Ext(path))
+	}
+	if _, err := os.Stat(path); err != nil {
+		return "", errors.New("parser: file" + path + "does not exist")
+	}
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	return string(bytes), nil
+}
